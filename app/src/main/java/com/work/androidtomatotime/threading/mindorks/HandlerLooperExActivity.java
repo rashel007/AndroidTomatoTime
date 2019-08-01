ea3901585d60d1b9c466bbde770f33ff94513457
EA Rashel <rashel320@gmail.com>
2019-08-01 23:33:53 +0600

    Create object of simpleworker and run multiple task and update textview in main thread

--- app/src/main/java/com/work/androidtomatotime/threading/mindorks/HandlerLooperExActivity.java
+++ app/src/main/java/com/work/androidtomatotime/threading/mindorks/HandlerLooperExActivity.java
@@ -1,17 +1,76 @@
 package com.work.androidtomatotime.threading.mindorks;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.work.androidtomatotime.R;
 
 public class HandlerLooperExActivity extends AppCompatActivity {
-    
+
+    SimpleWorker mSimpleWorker;
+    TextView textView;
+
+    Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            textView.setText(msg.obj.toString());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textview);
+
+        mSimpleWorker = new SimpleWorker();
+
+        mSimpleWorker.execute(() -> {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Message message = Message.obtain();
+            message.obj = "1st Task Completed";
+            handler.sendMessage(message);
+
+        }).execute(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Message message = Message.obtain();
+            message.obj = "2st Task Completed";
+            handler.sendMessage(message);
+        }).execute(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Message message = Message.obtain();
+            message.obj = "3st Task Completed";
+            handler.sendMessage(message);
+            
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSimpleWorker.quit();
     }
 }
