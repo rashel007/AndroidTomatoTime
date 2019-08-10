480e83e2ce82f3a9ff791c4adacf2da1adb20220
EA Rashel <rashel320@gmail.com>
2019-08-10 15:16:54 +0600

    avoid memory leak using weakreference

--- app/src/main/java/com/work/androidtomatotime/threading/codinginflow/AsyncTaskActivity.java
+++ app/src/main/java/com/work/androidtomatotime/threading/codinginflow/AsyncTaskActivity.java
@@ -9,9 +9,11 @@ import android.widget.Toast;
 
 import com.work.androidtomatotime.R;
 
+import java.lang.ref.WeakReference;
+
 public class AsyncTaskActivity extends AppCompatActivity {
 
-    private ProgressBar progress_bar;
+    ProgressBar progress_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +24,33 @@ public class AsyncTaskActivity extends AppCompatActivity {
     }
 
     public void startAsyncTask(View v) {
-        ExampleAsyncTask asyncTask = new ExampleAsyncTask();
+        ExampleAsyncTask asyncTask = new ExampleAsyncTask(this);
         asyncTask.execute(10);
     }
 
 
-    private class ExampleAsyncTask extends AsyncTask<Integer, Integer, String> {
+    /**
+     * to avoid memory lead we make the class static .
+     * to access mainactivity ui element in asynctask we create a weakreference of main activity.
+     */
+    private static class ExampleAsyncTask extends AsyncTask<Integer, Integer, String> {
 
+        private WeakReference<AsyncTaskActivity> mainActivityWeakReference;
+
+         ExampleAsyncTask(AsyncTaskActivity activity) {
+            mainActivityWeakReference = new WeakReference<AsyncTaskActivity>(activity);
+        }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
-            progress_bar.setVisibility(View.VISIBLE);
+            AsyncTaskActivity activity = mainActivityWeakReference.get();
+
+            if (activity == null || activity.isFinishing())
+                return;
+
+            activity.progress_bar.setVisibility(View.VISIBLE);
         }
 
         @Override
@@ -60,16 +76,26 @@ public class AsyncTaskActivity extends AppCompatActivity {
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
 
-            progress_bar.setProgress(values[0]);
+            AsyncTaskActivity activity = mainActivityWeakReference.get();
+
+            if (activity == null || activity.isFinishing())
+                return;
+
+            activity.progress_bar.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            Toast.makeText(AsyncTaskActivity.this, s, Toast.LENGTH_SHORT).show();
-            progress_bar.setProgress(0);
-            progress_bar.setVisibility(View.INVISIBLE);
+            AsyncTaskActivity activity = mainActivityWeakReference.get();
+
+            if (activity == null || activity.isFinishing())
+                return;
+
+            Toast.makeText(activity, s, Toast.LENGTH_SHORT).show();
+            activity.progress_bar.setProgress(0);
+            activity.progress_bar.setVisibility(View.INVISIBLE);
         }
 
 
